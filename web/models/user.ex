d59c8e259274ce51a234bd2e90719b04dbc82789
Zach Toolson <zach.toolson@mx.com>
2017-05-24 16:05:27 -0600

    Create user model

---
+++ web/models/user.ex
@@ -0,0 +1,36 @@
+defmodule Spikey.User do
+  use Spikey.Web, :model
+
+  schema "users" do
+    field :first_name, :string
+    field :last_name, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :encrypted_password, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:first_name, :last_name, :email, :password])
+    |> validate_required([:email, :password])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 5)
+    |> validate_confirmation(:password, message: "Password does not match")
+    |> unique_constraint(:email, message: "Email already taken")
+    |> generate_encrypted_password
+  end
+
+  defp generate_encrypted_password(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: password}} ->
+        put_change(changeset, :encrypted_password, Comeonin.Bcrypt.hashpwsalt(password))
+      _ ->
+        changeset
+    end
+  end
+end
